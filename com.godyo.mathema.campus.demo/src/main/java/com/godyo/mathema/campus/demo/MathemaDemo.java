d8c4b094e62855570bb23e63a389d2e9cf99e1c1
Manuel Beckmann <manuel.beckmann@godyo.com>
2017-04-18 15:27:55 +0200

    Initialer Commit

---
+++ com.godyo.mathema.campus.demo/src/main/java/com/godyo/mathema/campus/demo/MathemaDemo.java
@@ -0,0 +1,60 @@
+package com.godyo.mathema.campus.demo;
+ 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class MathemaDemo extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+    	final String COMMAND_GREETING = "Sag Hallo!";
+    	final String COMMAND_SILENT = "So Ruhe jetzt!";
+
+    	primaryStage.setTitle("Hallo Mathema Demo!");
+        
+        Button btn = new Button();
+		btn.setText(COMMAND_GREETING);
+        Label textLabel = new Label();
+        textLabel.setContentDisplay(ContentDisplay.CENTER);
+        textLabel.setMinSize(300, 100);
+        textLabel.setAlignment(Pos.CENTER);
+        textLabel.setStyle("-fx-font-size: 15pt;");
+
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            	switch (btn.getText()) {
+            		case COMMAND_GREETING: 
+            			textLabel.setText("Hallo Mathema Campus");
+            			btn.setText(COMMAND_SILENT);
+            			break;
+            		case COMMAND_SILENT: 
+            			textLabel.setText("");
+            			btn.setText(COMMAND_GREETING);
+            	}
+            }
+        });
+        
+        
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(btn);
+        borderPane.setBottom(textLabel);
+        
+        primaryStage.setScene(new Scene(borderPane, 300, 250));
+        primaryStage.show();
+    }
+}
