6711184ff9612e06159237f0c8ca71df31a14645
Manuel Beckmann <manuel.beckmann@godyo.com>
2017-04-28 08:32:45 +0200

    Fix Test

--- com.godyo.mathema.campus.demo/src/main/java/com/godyo/mathema/campus/demo/GreetingsFactory.java
+++ com.godyo.mathema.campus.demo/src/main/java/com/godyo/mathema/campus/demo/GreetingsFactory.java
@@ -3,6 +3,9 @@ package com.godyo.mathema.campus.demo;
 public class GreetingsFactory {
 	
 	public String createGreeting(String name) {
+		if (name == null) {
+			throw new NullPointerException("name must not be null");
+		}
 		return "Hallo "+ name;
 	}
 
